3d918f992d6dc3bfac310de6bfaac78e7e34403f
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-05-30 22:51:24 +0300

    core logic in task1 complited

---
+++ task1/solution.py
@@ -0,0 +1,58 @@
+import inspect
+from functools import wraps
+
+
+def strict(func):
+    """
+    A decorator that enforces strict typing rules on function arguments.
+
+    Ensures that:
+        - All function parameters are explicitly annotated.
+        - No parameters have default values.
+        - Annotations only use allowed types: bool, int, float, str.
+
+    Args:
+        func : The function to validate and wrap.
+
+    Returns:
+        The wrapped function with validation logic.
+
+    Raises:
+        TypeError:
+            - If any parameter is missing an annotation.
+            - Any parameter has a default value.
+            - Any annotation is not in the allowed types.
+    """
+    @wraps(func)
+    def wrapped_func(*args, **kwargs):
+        TYPES_ALLOWED = (bool, int, float, str)
+
+        func_sign = inspect.signature(func)
+        parameters = func_sign.parameters
+
+        for name, param in parameters.items():
+            if param.default != inspect._empty:
+                raise TypeError(f"Argument {name} was passed with a default value")
+            elif param.annotation == inspect._empty:
+                raise TypeError(f"Argument {name} was passed without annotation")
+            elif param.annotation not in TYPES_ALLOWED:
+                raise TypeError(f"Type {param.annotation.__name__} of argument '{name}' is not allowed!")
+
+        return func(*args, **kwargs)
+
+    return wrapped_func
+
+
+@strict
+def sum_two(a: int, b: int):
+    """
+    Adds two objects together.
+
+    Args:
+        a : The first operand.
+        b : The second operand.
+
+    Returns:
+        The sum or concatenation of the two operands.
+    """
+    return a + b
---
+++ type_hints_decorator.py
@@ -0,0 +1,14 @@
+from task1.solution import sum_two
+
+
+def main() -> None:
+    """
+    Entry point of strict typing realisation.
+
+    Calls the `sum_two` function with two objects and prints the result.
+    """
+    print(sum_two(a=1, b=2))
+
+
+if __name__ == "__main__":
+    main()
