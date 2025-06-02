65b1083ec36f773e0d6fd425158d4148eeb97fd0
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-06-02 05:54:31 +0300

    updated test for sum_two func

--- tests/task1/conftest.py
+++ tests/task1/conftest.py
@@ -3,14 +3,27 @@ from pathlib import Path
 import pytest
 
 
-def load_cases_from_json(path, case_exp):
+def load_cases_from_json(path: Path, case_exp: str):
+    """
+    Loads test cases from a JSON file.
+
+    Reads the specified JSON file and yields test cases.
+
+    Args:
+        path : Path
+            The path to the JSON file containing test data.
+        case_exp : str
+            The key in the JSON file indicating the type of cases to load ("PASSED" or "FAILED").
+
+    Yields:
+        tuple
+            A tuple of (a, b, expected) for PASSED and FAILED cases,
+    """
     with open(path, "r") as jsonfile:
         data = json.load(jsonfile)
         for case in data[case_exp]:
-            if case_exp == "PASSED":
-                yield (case["a"], case["b"], case["expected"])
-            elif case_exp == "FAILED":
-                yield (case["a"], case["b"], case["expected_error"])
+            yield (case["a"], case["b"], case["expected"])
+
 
 
 @pytest.fixture(
@@ -18,6 +31,16 @@ def load_cases_from_json(path, case_exp):
     ids=lambda val: f"{val[0]}+{val[1]}={val[2]}",
 )
 def sum_two_case_passed(request):
+    """
+    Pytest fixture providing positive test cases.
+
+    Yields:
+        tuple
+            A tuple of (a, b, expected) where:
+                - a : The first operand.
+                - b : The second operand.
+                - expected : The expected result of sum_two(a, b).
+    """
     return request.param
 
 
@@ -26,4 +49,14 @@ def sum_two_case_passed(request):
     ids=lambda val: f"{val[0]}+{val[1]} raises {val[2]}",
 )
 def sum_two_case_failed(request):
+    """
+    Pytest fixture providing negative test cases.
+
+    Yields:
+        tuple
+            A tuple of (a, b, expected_error) where:
+                - a : The first operand.
+                - b : The second operand.
+                - expected : The name of the expected exception (as string).
+    """
     return request.param
