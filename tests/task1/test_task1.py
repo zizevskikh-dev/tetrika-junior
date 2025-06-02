c5bbc2044ce19e3f27e7eb3fda58721a29328346
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-06-02 05:21:31 +0300

    updated test for sum_two func

--- tests/task1/test_task1.py
+++ tests/task1/test_task1.py
@@ -1,3 +1,4 @@
+import builtins
 import pytest
 from task1.solution import strict, sum_two
 
@@ -9,5 +10,6 @@ def test_sum_two(sum_two_case_passed):
 
 def test_sum_two_failures(sum_two_case_failed):
     a, b, expected_error = sum_two_case_failed
-    with pytest.raises(eval(expected_error)):
+    exception_class = getattr(builtins, expected_error)
+    with pytest.raises(exception_class):
         sum_two(a, b)
